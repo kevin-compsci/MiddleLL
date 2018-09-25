fcc382b0a4a373114ea75ec8b0ac20a94ba2a31d
Kevin Nguyen <25444103+kevin-compsci@users.noreply.github.com>
2018-09-24 22:23:52 -0500

    Add files via upload

---
+++ MiddleLL/Java/JavaMLL.java
@@ -0,0 +1,54 @@
+/*
+Kevin Nguyen
+Basic Java program to get middle nodes (and beyond).
+
+How to run (linux):
+	1.) open command line and cd into directory folder of where this code is
+	2.) type in "javac *.java" to compile java files
+	3.) type in "java JavaMLL"
+*/
+
+//imports
+import java.io.*;
+import java.util.*;
+
+//class
+class JavaMLL {
+	//Global Declarations
+
+	//main driver
+	public static void main(String[] args) {
+		//local Declarations
+		
+		//initialize LinkedList
+
+		//call function (returns a new LL)
+
+		//print results
+	}
+
+	//function to return the middle of a LL
+	// public ListNode middleNode(ListNode head) {
+ //        //local declarations
+ //        ListNode tempFast = head, tempSlow = head;
+ //        int count = 1;
+ //        //loop over all nodes
+ //        while(tempFast != null) {
+ //            //conditional to set single/double iterations over nodes
+ //            if(count == 1) {
+ //                tempFast = tempFast.next;
+ //                count++;
+ //            }
+ //            else {
+ //                tempSlow = tempSlow.next;
+ //                if(tempFast.next == null) {
+ //                    tempFast = tempFast.next;
+ //                }
+ //                else {
+ //                    tempFast = tempFast.next.next;                
+ //                }
+ //            }
+ //        }        
+ //        return tempSlow;        
+ //    }
+}
